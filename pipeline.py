9b4810f102433dfb3eaa7d389ced42897cbd67bc
DMatvei <matvei.dranitsyn@yandex.ru>
2026-04-14 14:25:55 +1000

    add check wav file

--- pipeline.py
+++ pipeline.py
@@ -5,9 +5,12 @@ def process(input_path: str, output_dir: str, on_progress=None):
     os.makedirs(output_dir, exist_ok=True)
 
     # конвертация
+    if input_path.lower().endswith(".wav"):
+        wav_path = input_path
+    else:
+        wav_path = os.path.join(output_dir, "input.wav")
+        convert_to_wav(input_path, wav_path)
 
-    wav_path = os.path.join(output_dir, "input.wav")
-    convert_to_wav(input_path, wav_path)
     if on_progress: on_progress(20)
 
 
